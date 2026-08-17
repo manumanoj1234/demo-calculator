cd091705c0491eb004b1dd652b357cad2eda743b
Manoj M <manu17630@gmail.com>
2026-08-17 13:01:47 +0530

    Add Program.cs (calls Add and Subtract defined outside Main)

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace DemoCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 4;
+
+            int sum = Calculator.Add(a, b);
+            int diff = Calculator.Subtract(a, b);
+
+            Console.WriteLine($"Add({a}, {b}) = {sum}");
+            Console.WriteLine($"Subtract({a}, {b}) = {diff}");
+        }
+    }
+}
