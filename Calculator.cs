3bc5a88edc843c928fe3e106e2b35019b151d1c5
Manoj M <manu17630@gmail.com>
2026-08-17 13:11:50 +0530

    Remove arithmetic methods from Calculator class
    Removed subtraction, multiplication, division, and modulo methods along with their documentation.

--- Calculator.cs
+++ Calculator.cs
@@ -17,59 +17,5 @@ namespace DemoCalculator
         {
             return a + b;
         }
-
-        /// <summary>
-        /// Subtracts two numbers.
-        /// </summary>
-        /// <param name="a">First number</param>
-        /// <param name="b">Second number</param>
-        /// <returns>The difference of a and b</returns>
-        public static double Subtract(double a, double b)
-        {
-            return a - b;
-        }
-
-        /// <summary>
-        /// Multiplies two numbers.
-        /// </summary>
-        /// <param name="a">First number</param>
-        /// <param name="b">Second number</param>
-        /// <returns>The product of a and b</returns>
-        public static double Multiply(double a, double b)
-        {
-            return a * b;
-        }
-
-        /// <summary>
-        /// Divides two numbers.
-        /// </summary>
-        /// <param name="a">Dividend</param>
-        /// <param name="b">Divisor</param>
-        /// <returns>The quotient of a and b</returns>
-        /// <exception cref="ArgumentException">Thrown when b is zero</exception>
-        public static double Divide(double a, double b)
-        {
-            if (b == 0)
-            {
-                throw new ArgumentException("Division by zero is not allowed.", nameof(b));
-            }
-            return a / b;
-        }
-
-        /// <summary>
-        /// Calculates the remainder of division.
-        /// </summary>
-        /// <param name="a">Dividend</param>
-        /// <param name="b">Divisor</param>
-        /// <returns>The remainder of a divided by b</returns>
-        /// <exception cref="ArgumentException">Thrown when b is zero</exception>
-        public static double Modulo(double a, double b)
-        {
-            if (b == 0)
-            {
-                throw new ArgumentException("Division by zero is not allowed.", nameof(b));
-            }
-            return a % b;
-        }
     }
 }
