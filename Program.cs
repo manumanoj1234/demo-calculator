f267fc0953ac2867458874d2195c4b3a5dc923bd
Athul <athulantonyvs@email.com>
2026-08-18 09:43:29 +0530

    arithmetic features added in calculator.cs

--- Program.cs
+++ Program.cs
@@ -9,9 +9,15 @@ namespace DemoCalculator
             int a = 10;
             int b = 4;
             int c = 10;
-            int sum = Calculator.Add(a, b);
-            
+            double sum = Calculator.Add(a, b);
+            double diff = Calculator.Sub(a, b);
+            double multi = Calculator.Multi(a, b);
+            double div = Calculator.Div(a, b);
+
             Console.WriteLine($"Add({a}, {b}) = {sum}");
-       }
+            Console.WriteLine($"Sub({a}, {b}) = {diff}");
+            Console.WriteLine($"Multi({a}, {b}) = {multi}");
+            Console.WriteLine($"Div({a}, {b}) = {div}");
+        }
     }
 }
